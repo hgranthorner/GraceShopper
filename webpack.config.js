569da8145721163196252665875ca67b2aee3c50
Grant Horner <h.grant.horner@gmail.com>
2019-05-01 17:43:31 -0400

    fixed front end

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'js', 'client', 'src', 'index.tsx'),
+  entry: path.resolve(__dirname, 'js', 'client', 'src', 'index.js'),
   devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, 'js', 'client', 'dist'),
+    path: path.resolve(__dirname, 'src', 'client', 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
-  },
-};
+  }
+}
