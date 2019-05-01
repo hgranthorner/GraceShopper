6a4789b36da9bf158feb7fe6e890b3e54fcb7b81
Grant Horner <h.grant.horner@gmail.com>
2019-05-01 18:32:21 -0400

    Added seeding

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 To get started, run "npm i" and "createdb graceshopper".
 
+Run "npm run seed" to put basic data in the db.
+
 Then, run either "npm run webpack" for the front end, "npm run build-server" for the back end,
 or "npm run start:dev" to build everything at once.
--- src/server/seed.ts
+++ src/server/seed.ts
@@ -3,17 +3,50 @@ import User from './models/user'
 import Product from './models/product'
 import Category from './models/category'
 import conn from './db'
-import { sync } from './db'
 
-export default () => {
-  return conn
-    .sync({ force: true })
-    .then(() => {
-      const user = new User({ name: 'Bailie', age: 10 })
-      return user.save()
-    })
-    .then(() => {
-      console.log('Completed seeding.')
-    })
-    .catch((e: Error) => console.log(`Failed to seed. Here's why:\n${e}`))
-}
+conn
+  .sync({ force: true })
+  .then(() => {
+    return Promise.all([
+      new User({ name: 'Bailie', age: 10 }).save(),
+      new User({ name: 'Dan', age: 20 }).save(),
+      new User({ name: 'Grant', age: 30 }).save()
+    ])
+  })
+  .then(() => {
+    console.log('Completed seeding users.')
+  })
+  .then(() => {
+    return Promise.all([
+      new Category({ name: 'Electronics', description: 'sweet zip zaps' }).save(),
+      new Category({ name: 'Household', description: 'clean that ish up' }).save(),
+      new Category({ name: 'Cars', description: 'vroom vroom' }).save()
+    ])
+  })
+  .then(() => {
+    console.log('Completed seeding categories.')
+  })
+  .then(() => {
+    return Promise.all([
+      new Product({ name: 'Phone', price: 5, description: 'ring ring', imageUrl: '', quantity: 1, categoryId: 1 }).save(),
+      new Product({ name: 'TV', price: 10, description: 'zombify your brain for fun', imageUrl: '', quantity: 2, categoryId: 1 }).save(),
+      new Product({
+        name: 'Headphones',
+        price: 100,
+        description: 'literally ignore the rest of the world',
+        imageUrl: '',
+        quantity: 3,
+        categoryId: 1
+      }).save(),
+      new Product({ name: 'Glad', price: 5, description: 'get glad', imageUrl: '', quantity: 1, categoryId: 2 }).save(),
+      new Product({ name: 'Vacuum', price: 10, description: 'vroom', imageUrl: '', quantity: 2, categoryId: 2 }).save(),
+      new Product({ name: 'Sponge', price: 100, description: 'squish squish', imageUrl: '', quantity: 3, categoryId: 2 }).save(),
+      new Product({ name: 'Audi', price: 1000, description: 'fancy a$$ bish', imageUrl: '', quantity: 3, categoryId: 3 }).save(),
+      new Product({ name: 'Toyota', price: 100, description: 'tryna drive uber', imageUrl: '', quantity: 3, categoryId: 3 }).save(),
+      new Product({ name: 'Tesla', price: 100, description: 'I love Musk', imageUrl: '', quantity: 3, categoryId: 3 }).save()
+    ])
+  })
+  .then(() => {
+    console.log('Completed seeding products.')
+  })
+  .catch((e: Error) => console.log(`Failed to seed. Here's why:\n${e}`))
