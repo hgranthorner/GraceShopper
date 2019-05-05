bd34c360c5a500974517d5ad235fea3f513589ca
Grant Horner <h.grant.horner@gmail.com>
2019-05-05 12:58:05 -0400

    set up basic productlist

---
+++ src/client/src/components/Home/ProductList/ProductItem.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Product } from 'src/@types/redux-types'
+import { Link } from 'react-router-dom'
+
+const ProductItem = ({ product }: { product: Product }) => {
+  const deleteProduct = () => {
+    return
+  }
+  return (
+    <div>
+      <div className="row">
+        <div className="col">
+          <Link to={`/products/${product.id}`}>
+            <h3>{product.name}</h3>
+          </Link>
+          <h6>
+            <span>${product.price}</span>
+          </h6>
+        </div>
+        <div className="col d-flex flex-row-reverse align-items-center">
+          <div className="btn-group">
+            <Link to={`/products/update/${product.id}`}>
+              <button className={'btn btn-info'} type={'button'}>
+                Update
+              </button>
+            </Link>
+            <button className={'btn btn-danger'} type={'button'} onClick={() => deleteProduct()}>
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <img alt={'A picture of a product'} src={product.imageUrl} className={'col'} />
+        <p className={'col'}>{product.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProductItem
