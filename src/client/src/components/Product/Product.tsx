e468997638b2deb0fff0376555476e9733c39047
Bailie Wu <wubailie@gmail.com>
2019-05-05 22:45:26 -0400

    added basic products component

---
+++ src/client/src/components/Product/Product.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div className="d-flex justify-content-center">
+      <p>Here goes the product-details.</p>
+    </div>
+  )
+}
--- src/client/src/components/index.tsx
+++ src/client/src/components/index.tsx
@@ -1,5 +1,6 @@
 import Nav from './Nav/Nav'
 import Home from './Home/Home'
 import Footer from './Footer/Footer'
+import Product from './Product/Product'
 
-export { Nav, Home, Footer }
+export { Nav, Home, Footer, Product }
