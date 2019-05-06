039a8549a8567ded3b27283fcdf13a92822c8c19
Bailie Wu <wubailie@gmail.com>
2019-05-06 00:26:00 -0400

    added some text formatting. still need real formatting

--- src/client/src/components/Product/Product.tsx
+++ src/client/src/components/Product/Product.tsx
@@ -15,10 +15,12 @@ const Product = ({ product, fetchProduct, match }: { product: Product; fetchProd
   useEffect(() => {
     fetchProduct(match.params.id)
   })
+  // use below line to format price to include commas.
+  // (12345.67).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');  // 12,345.67
   return (
-    <div className="d-flex justify-content-center">
+    <div className="row">
       {product.name}
-      <h6>${product.price}, {product.quantity} available</h6>
+      <h6>${product.price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} {product.quantity.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, '$&,').slice(0, -2)} available</h6>
       <img alt={'A picture of the product'} src={product.imageUrl} className="col"></img>
       <p>{product.description}</p>
 
