d8b84b38c9a607134dc8b7ecba44e64516872a77
dm1031 <danmdrums@gmail.com>
2019-05-06 18:13:00 -0400

    added links to nav

--- src/client/src/components/Nav/Nav.tsx
+++ src/client/src/components/Nav/Nav.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import Search from './Search'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
+  console.log('hello')
+  const links = [
+    {
+      type: 'Home',
+      path: '/'
+    },
+    {
+      type: 'Cart',
+      path: '/users/:id/orders'
+    },
+    {
+      type: 'Browse by category',
+      path: '/categories'
+    },
+    {
+      type: 'Browse by product',
+      path: '/products'
+    }
+  ]
   return (
     <div className="d-flex justify-content-around">
-      <h1>Welcome to Bailie's Beauts</h1>
+      <h1>Welcome to Bailie'ssss Beauts</h1>
+      <ul>
+        {links.map(link => {
+          ;<Link to={link.path}>{link.type}</Link>
+        })}
+      </ul>
       <Search />
     </div>
   )
