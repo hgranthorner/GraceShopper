87449257ff586f2e9c726b2842ace316778c48ba
Grant Horner <h.grant.horner@gmail.com>
2019-05-08 20:40:43 -0400

    created searchbar

--- src/client/src/components/Nav/Search.tsx
+++ src/client/src/components/Nav/Search.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { updateSearchTerm } from '../../store'
+import { connect } from 'react-redux'
 
-export default function Search() {
+const mapStateToProps = ({ searchTerm }: { searchTerm: string }) => ({ searchTerm })
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    updateSearchTerm: (searchTerm: string) => dispatch(updateSearchTerm(searchTerm))
+  }
+}
+
+const Search = ({ searchTerm, updateSearchTerm }: { searchTerm: string; updateSearchTerm: any }) => {
   return (
-    <div>
-      <p>Search bar goes here</p>
+    <div className={'input-group'}>
+      <input
+        placeholder="Search Results"
+        className="form-control"
+        value={searchTerm}
+        onChange={evt => updateSearchTerm(evt.target.value)}
+      />
     </div>
   )
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search)
