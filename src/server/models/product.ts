b30e175c53ec51b640d94a1e4937f5ca33a2388c
Grant Horner <h.grant.horner@gmail.com>
2019-05-09 20:41:41 -0400

    Implemented search

--- src/server/models/product.ts
+++ src/server/models/product.ts
@@ -1,11 +1,4 @@
-import {
-  Column,
-  Model,
-  Table,
-  DataType,
-  ForeignKey,
-  BelongsTo
-} from 'sequelize-typescript'
+import { Column, Model, Table, DataType, ForeignKey, BelongsTo } from 'sequelize-typescript'
 import Category from './category'
 import Order from './order'
 
