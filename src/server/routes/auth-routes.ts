92ee6a688731c384c7571eaaf99c2db3fdfceee6
Grant Horner <h.grant.horner@gmail.com>
2019-05-11 18:19:33 -0400

    some refactoring

--- src/server/routes/auth-routes.ts
+++ src/server/routes/auth-routes.ts
@@ -4,51 +4,40 @@ import Order from '../models/order'
 import Product from '../models/product'
 const router = express.Router()
 
-router.put(
-  '/login',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log('request body', req.body)
-    User.findOne({
-      where: {
-        name: req.body.name,
-        password: req.body.password
+router.put('/login', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log('request body', req.body)
+  User.findOne({
+    where: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+    .then(async user => {
+      console.log('this is the user', user!.get())
+      if (!user) {
+        res.sendStatus(401)
       }
+      if (req.session!.order) {
+        console.log('preparing to create orders')
+        console.log(`Here's the userId: `, user!.id)
+        const userId: number = user!.id
+        const order = req.session!.order
+        Order.addToCart(userId, order[0].id)
+          .then(() => console.log('created first order'))
+          .then(() => {
+            for (let i = 1; i < order.length; i++) {
+              Order.addToCart(userId, order[i].id) //
+                .then(() => console.log(`Added order #${i}`))
+            }
+          })
+          .then(() => (req.session!.order = []))
+          .then(() => console.log(`req.session after loading orders\n${req.session!.toString()}`))
+          .then(() => res.send(user))
+          .catch((e: Error) => console.log(`Some error happened\n${e}`))
+      }
+      res.send(user)
     })
-      .then(user => {
-        console.log('this is the user', user!.get())
-        if (!user) {
-          res.sendStatus(401)
-        } else {
-          console.log('Creating an order')
-          console.log(`Here's the userId: `, user!.id)
-          const userId: number = user.id
-          console.log('now on line 25')
-          console.log(`Here's the current session: `, req.session)
-          if (req.session!.order) {
-            const order = req.session!.order
-            console.log('preparing to create orders')
-            Order.addToCart(userId, order[0].id)
-              .then(() => console.log('created first order'))
-              .then(() => {
-                for (let i = 1; i < order.length; i++) {
-                  Order.addToCart(userId, order[i].id) //
-                    .then(() => console.log(`Added order #${i}`))
-                }
-              })
-              .then(() => (req.session!.order = []))
-              .then(() =>
-                console.log(
-                  `req.session after loading orders\n${req.session!.toString()}`
-                )
-              )
-              .then(() => res.send(user))
-              .catch((e: Error) => console.log(`Some error happened\n${e}`))
-          }
-          res.send(user)
-        }
-      })
-      .catch(next)
-  }
-)
+    .catch(next)
+})
 
 export default router
