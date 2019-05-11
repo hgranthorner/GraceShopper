7533a8a08578cc0055c8ef2dec7583b1c8fe0b16
Grant Horner <h.grant.horner@gmail.com>
2019-05-11 19:35:38 -0400

    restyled products

--- src/client/src/components/Home/ProductList/ProductItem.tsx
+++ src/client/src/components/Home/ProductList/ProductItem.tsx
@@ -3,36 +3,41 @@ import { Product } from 'src/@types/redux-types'
 import { Link } from 'react-router-dom'
 
 const ProductItem = ({ product }: { product: Product }) => {
-  const deleteProduct = () => {
-    return
-  }
   return (
     <div className="border mb-2">
       <div className="row">
-        <div className="col">
-          <Link to={`/products/${product.id}`}>
-            <h3>{product.name}</h3>
-          </Link>
-          <h6>
-            <span>${product.price}</span>
-          </h6>
+        <div className="col product-image">
+          <img alt="A picture of a product" src={product.imageUrl} className="col pt-1 pb-1 pl-1" />
         </div>
-        <div className="col d-flex flex-row-reverse align-items-center">
-          <div className="btn-group">
-            <Link to={`/products/update/${product.id}`}>
-              <button className={'btn btn-info'} type={'button'}>
-                Update
-              </button>
+        <div className="col d-flex align-items-around justify-content-center flex-column">
+          <div className="row d-flex align-items-center justify-content-around">
+            <Link to={`/products/${product.id}`}>
+              <h3>{product.name}</h3>
             </Link>
-            <button className={'btn btn-danger'} type={'button'} onClick={() => deleteProduct()}>
-              Remove
-            </button>
+            <h6>
+              <span>${product.price}</span>
+            </h6>
+          </div>
+          <div className="row">
+            <div className="d-flex flex-row align-items-center">
+              <div className="col">
+                <p>{product.description}</p>
+              </div>
+              <div className="col">
+                <div className="btn-group">
+                  <Link to={`/products/update/${product.id}`}>
+                    <button className="btn btn-info" type="button">
+                      Update
+                    </button>
+                  </Link>
+                  <button className="btn btn-danger" type="button">
+                    Remove
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <img alt={'A picture of a product'} src={product.imageUrl} className={'col'} />
-        <p className={'col'}>{product.description}</p>
       </div>
     </div>
   )
