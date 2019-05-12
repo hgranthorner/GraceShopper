6f3ffb4221b7156b566b4b3bd78835eda23bf43d
Grant Horner <h.grant.horner@gmail.com>
2019-05-12 12:20:25 -0400

    fixed bug: posting to users now works

--- src/server/routes/api/orders.ts
+++ src/server/routes/api/orders.ts
@@ -2,36 +2,4 @@ import express = require('express')
 import Order from '../../models/order'
 const route = express.Router()
 
-route.post(
-  '/users/:userId/orders',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log('post route hit')
-    // Check if a user is logged in.
-    if (req.params.userId === -1) {
-      console.log('no user logged in')
-      // No user. We have ourselves a guest. Guest
-      // will add items to cart and the data will
-      //be stored on the session.
-
-      // Check if the session already contains
-      // an order.
-      if (!req.session!.order) {
-        // If there is no order on the session, initialize one.
-        // Subsequent selected products will be stored here.
-        req.session!.order = []
-      }
-      req.session!.order.push(req.body)
-      res.send(req.session!.order)
-    } else {
-      // We have a logged in user, and a selected product.
-      const userId = req.params.userId
-      const product = req.body
-      // Find the associated order that exists as an open cart,
-      // and add to that cart.
-      return Order.addToCart(userId, product.id)
-    }
-    next()
-  }
-)
-
 export default route
--- src/server/routes/api/users.ts
+++ src/server/routes/api/users.ts
@@ -1,6 +1,6 @@
 import express = require('express')
 import User from '../../models/user'
-import Order from '../../models/product'
+import Order from '../../models/order'
 const route = express.Router()
 
 // get orders associated with a user by id
@@ -17,6 +17,38 @@ route.get('/:id/orders', (req: express.Request, res: express.Response, next: exp
   })
     .then(user => res.send(user))
     .catch(next)
+})
+
+route.post('/:userId/orders', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log('post route hit')
+  // Check if a user is logged in.
+  console.log(req.params.userId)
+  if (req.params.userId === '-1') {
+    console.log('no user logged in')
+    // No user. We have ourselves a guest. Guest
+    // will add items to cart and the data will
+    //be stored on the session.
+
+    // Check if the session already contains
+    // an order.
+    if (!req.session!.order) {
+      // If there is no order on the session, initialize one.
+      // Subsequent selected products will be stored here.
+      req.session!.order = []
+    }
+    req.session!.order.push(req.body)
+    res.send(req.session!.order)
+  } else {
+    console.log('found a user')
+    // We have a logged in user, and a selected product.
+    const userId = req.params.userId
+    console.log(userId)
+    const product = req.body
+    // Find the associated order that exists as an open cart,
+    // and add to that cart.
+    return Order.addToCart(userId, product.id)
+  }
+  next()
 })
 
 export default route
