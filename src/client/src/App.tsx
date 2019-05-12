4f08002526462004a18a4874ebb12b2c713d4773
dm1031 <danmdrums@gmail.com>
2019-05-11 22:02:02 -0400

    merge with Grant changes

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,25 +1,5 @@
 import React, { useEffect } from 'react'
 import { HashRouter, Route, Switch } from 'react-router-dom'
-<<<<<<< HEAD
-import { Home, Nav, Footer, Product, Login, Order } from './components'
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <HashRouter>
-        <Nav />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/categories/" component={Home} />
-          <Route path="/products/:id" exact component={Product} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/orders" exact component={Order} />
-        </Switch>
-        <Footer />
-      </HashRouter>
-    )
-  }
-=======
 import { Home, Nav, Footer, Product, Login } from './components'
 import { connect } from 'react-redux'
 import { checkIfLoggedIn } from './store'
@@ -33,7 +13,13 @@ const mapDispatchToProps = (dispatch: any) => ({
   checkIfLoggedIn: () => dispatch(checkIfLoggedIn())
 })
 
-const App = ({ user, checkIfLoggedIn }: { user: User; checkIfLoggedIn: any }) => {
+const App = ({
+  user,
+  checkIfLoggedIn
+}: {
+  user: User
+  checkIfLoggedIn: any
+}) => {
   useEffect(() => {
     if (user.id === -1) checkIfLoggedIn()
   })
@@ -50,7 +36,6 @@ const App = ({ user, checkIfLoggedIn }: { user: User; checkIfLoggedIn: any }) =>
       <Footer />
     </HashRouter>
   )
->>>>>>> bba20c860ec93cfb710a7972fc6115cb39b5f93a
 }
 
 export default connect(
