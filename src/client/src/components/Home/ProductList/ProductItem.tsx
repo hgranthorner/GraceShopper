9a23e159b3decdf20fc7cd74e4b6b13b8414b288
Grant Horner <h.grant.horner@gmail.com>
2019-05-12 11:41:33 -0400

    fixed product item html/css

--- src/client/src/components/Home/ProductList/ProductItem.tsx
+++ src/client/src/components/Home/ProductList/ProductItem.tsx
@@ -10,8 +10,7 @@ const mapStateToProps = ({ user, order }: { user: User; order: Order }) => {
 
 const mapDispatchToProps = (dispatch: any) => {
   return {
-    userAddToOrder: (userId: number, product: Product) =>
-      dispatch(loggedInAddToOrder(userId, product)),
+    userAddToOrder: (userId: number, product: Product) => dispatch(loggedInAddToOrder(userId, product)),
     guestAddToOrder: (product: Product) => dispatch(createOrder(product))
   }
 }
@@ -36,48 +35,32 @@ const ProductItem = ({
     <div className="border mb-2">
       <div className="row">
         <div className="col product-image">
-          <img
-            alt="A picture of a product"
-            src={product.imageUrl}
-            className="col pt-1 pb-1 pl-1"
-          />
+          <img alt="A picture of a product" src={product.imageUrl} className="col pt-1 pb-1 pl-1" />
         </div>
         <div className="col d-flex align-items-around justify-content-center flex-column">
           <div className="row d-flex align-items-center justify-content-around">
-            <Link to={`/products/${product.id}`}>
-              <h3>{product.name}</h3>
-            </Link>
-            <h6>
-              <span>${product.price}</span>
-            </h6>
+            <div className="col d-flex justify-content-center">
+              <Link to={`/products/${product.id}`}>
+                <h3>{product.name}</h3>
+              </Link>
+            </div>
+            <div className="col d-flex justify-content-center">
+              <h6>
+                <span>${product.price}</span>
+              </h6>
+            </div>
           </div>
-          <div className="row">
-            <div className="d-flex flex-row align-items-center">
-              <div className="col">
-                <p>{product.description}</p>
-              </div>
-              <div className="col">
-                <div className="btn-group">
-                  <button
-                    className={'btn btn-success'}
-                    onClick={() =>
-                      user.id === -1
-                        ? guestAddToOrder(product)
-                        : userAddToOrder(user.id, product)
-                    }
-                  >
-                    +Cart
-                  </button>
-                  <Link to={`/products/update/${product.id}`}>
-                    <button className="btn btn-info" type="button">
-                      Update
-                    </button>
-                  </Link>
-                  <button className="btn btn-danger" type="button">
-                    Remove
-                  </button>
-                </div>
-              </div>
+          <div className="row d-flex flex-row align-items-center justify-content-around">
+            <div className="col d-flex justify-content-center">
+              <p>{product.description}</p>
+            </div>
+            <div className="col d-flex justify-content-center">
+              <button
+                className={'btn btn-raised btn-success'}
+                onClick={() => (user.id === -1 ? guestAddToOrder(product) : userAddToOrder(user.id, product))}
+              >
+                +Cart
+              </button>
             </div>
           </div>
         </div>
