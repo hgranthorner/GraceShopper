25034fffb3204902a880cc26d6e47b0a54991261
hgranthorner <37941012+hgranthorner@users.noreply.github.com>
2019-05-12 15:28:05 -0400

    Revert "Delete from cart and more."

--- src/server/models/order.ts
+++ src/server/models/order.ts
@@ -32,103 +32,29 @@ class Order extends Model<Order> {
   @BelongsToMany(() => Product, () => OrdersProducts)
   products!: Product[]
 
-  static addToCart(userId: number, productId: number, quantity: number = 1) {
-    console.log(`Add to Cart: User|Product|Qty: ${userId}|${productId}|${quantity}`)
-    // 1. no orders: make a first cart
-    // 2. there are orders, but no cart: make a cart
-    // 3. there is a cart, find one
-    return Order.findOne({
+  static addToCart(userId: number, productId: number) {
+    return Order.findAll({
       where: {
         userId,
         status: Status.Cart
       }
     })
-      .then(async orderCart => {
+      .then(async orders => {
         let cart
-        if (!orderCart) {
-          console.log(`cannot find cart for userId ${userId}: creating one`)
+        if (orders.length === 0) {
+          console.log('cannot find order: creating one')
           cart = await Order.create({ userId, status: Status.Cart })
-        } else cart = orderCart
-        // else {
-        //   cart = orders.find(order => order.status === Status.Cart)
-        //   if (!cart) {
-        //     console.log(`cannot find cart for userId : creating one`)
-        //     cart = await Order.create({ userId, status: Status.Cart })
-        //   }
-        // }
+        } else {
+          cart = orders.find(order => order.status === Status.Cart)
+          if (!cart) {
+            console.log('cannot find cart: creating one')
+            cart = await Order.create({ userId, status: Status.Cart })
+          }
+        }
         return cart
       })
-      // 4. find existing ordersProduct instance and increase quantity by quantity.
-      // 5. if no existing ordersProduct instance, create instance.
-      .then(cart => {
-        // cart = order with status cart
-        return OrdersProducts.findAll({
-          where: {
-            orderId: cart.id
-          }
-        }).then(async (userOrderCart) => {
-          // userOrderCart is the orderProducts for the user's cart.
-          // If the productId exists, add quantity, else create product with that qty.
-          let foundProductFromCart = userOrderCart.find(orderProdLineItem => orderProdLineItem.productId === productId)
-          if (!foundProductFromCart) {
-            // product does not exist in cart. add product to order
-            return await OrdersProducts.create({ orderId: cart.id, productId: productId, quantity: quantity })
-          } else {
-            // product exists in cart. increase quantity
-            return await foundProductFromCart.update({ quantity: foundProductFromCart.quantity + quantity })
-          }
-        })
-      })
-      .catch((e: Error) => console.log(`Failed to add to cart. \n${e}`))
-  }
-
-  // Removes a product from cart. if optional paramter removeAll=true, then it removes all quantity of this item from the cart
-  static decreaseProductQuantityFromCart(userId: number, productId: number, removeAll: boolean = false) {
-    // find user's cart
-    return Order.findOne({
-      where: {
-        userId: userId,
-        status: Status.Cart
-      }
-    })
-      .then(cart => {
-        if (!cart) throw new Error(`User ${userId} has no cart. Should not empty a nonexistant cart.`)
-        return OrdersProducts.findOne({
-          where: {
-            orderId: cart.id,
-            productId: productId
-          }
-        })
-          .then(async (orderProductLineItem) => {
-            if (!orderProductLineItem) throw new Error(`Product ${productId} does not exist in cart.`)
-            if (orderProductLineItem.quantity === 1 || removeAll) return await orderProductLineItem.destroy()
-            return await orderProductLineItem.update({ quantity: orderProductLineItem.quantity - 1 })
-          })
-          .catch(er => console.log(`Failed to ${removeAll ? 'delete' : 'decrement'} product ${productId} from ${userId}'s cart.`))
-      })
-  }
-
-  static emptyCart(userId: number) {
-    // removes all items from a user's cart.
-    // finds a user's cart. removes all associated orderProduct 
-    return Order.findOne({
-      where: {
-        userId: userId,
-        status: Status.Cart
-      }
-    })
-      .then(cart => {
-        if (!cart) throw new Error(`User ${userId} has no cart. Should not empty a nonexistant cart`)
-        return OrdersProducts.findAll({
-          where: {
-            orderId: cart.id
-          }
-        })
-          .then(cartItems => {
-            return cartItems.map(cartItem => cartItem.destroy())
-          })
-      })
-      .catch(er => console.log(`Failed to empty cart.\n${er}`))
+      .then(cart => OrdersProducts.create({ orderId: cart.id, productId: productId }))
+      .then(lineItem => lineItem.orderId)
   }
 }
 
--- src/server/models/ordersProducts.ts
+++ src/server/models/ordersProducts.ts
@@ -1,31 +1,19 @@
-import {
-    Column,
-    Model,
-    Table,
-    ForeignKey,
-    DataType
-} from 'sequelize-typescript'
+import { Column, Model, Table, ForeignKey } from 'sequelize-typescript'
 import Product from './product'
 import Order from './order'
 
 @Table({
-    timestamps: true,
-    tableName: 'orders_products'
+  timestamps: true,
+  tableName: 'orders_products'
 })
 class OrdersProducts extends Model<OrdersProducts> {
-    @ForeignKey(() => Product)
-    @Column
-    productId!: number
+  @ForeignKey(() => Product)
+  @Column
+  productId!: number
 
-    @ForeignKey(() => Order)
-    @Column
-    orderId!: number;
-
-    @Column({
-        type: DataType.INTEGER,
-        allowNull: false
-    })
-    quantity!: number
+  @ForeignKey(() => Order)
+  @Column
+  orderId!: number
 }
 
 export default OrdersProducts
